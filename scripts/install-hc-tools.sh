5cd2c13c3dc62870cc74d7317b932ca842199229
Wesley Finck <wesleyfinck@gmail.com>
2024-12-06 15:43:22 -0800

    point to hdk_crud 14

--- scripts/install-hc-tools.sh
+++ scripts/install-hc-tools.sh
@@ -5,4 +5,4 @@ rustup target install wasm32-unknown-unknown
 
 # install `hc` cli tool
 # KEEP THIS IN SYNC
-cargo install holochain_cli --version 0.4.0-rc --locked
+cargo install holochain_cli --version 0.4.0-rc.0 --locked
